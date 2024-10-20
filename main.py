6fccff55f02a7440148a11d326f48a03efacf28c
Paweł <174018720+PilarToZiomal@users.noreply.github.com>
2024-10-20 18:30:58 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 
 from src.preproc import main as preproc_main
 from src.eda import main as eda_main
+from src.clustering import main as clustering_main
 
 def main():
     """Główny punkt wejścia do projektu."""
@@ -12,6 +13,10 @@ def main():
     print("Rozpoczynanie analizy EDA...")
     eda_main()
     print("\nAnaliza EDA zakończona.")
+
+    print("Rozpoczynanie klasteryzacji danych...")
+    clustering_main()
+    print("\nKlasteryzacja zakończona.")
 
 if __name__ == "__main__":
     main()
